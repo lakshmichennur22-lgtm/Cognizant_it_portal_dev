f03ef8d9cf61cd3c1317ec1f8062d33164957a60
lakshmi <lakshmichennur22@gmail.com>
2025-12-05 16:45:55 +0530

    initial front app created

---
+++ my-app/src/components/info.js
@@ -0,0 +1,12 @@
+import './info.css';
+
+function info() {
+    return (
+        <section className="info" id="info">
+            <h2>info</h2>
+            <p>Cognisant SoftSol is an IT consulatancy company that delivers modern web solutions, training, and support for our clients.</p>
+        </section>
+    );
+}
+
+export default info;
